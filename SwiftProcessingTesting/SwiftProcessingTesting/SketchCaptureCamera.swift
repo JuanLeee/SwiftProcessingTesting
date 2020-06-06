f25aa7adbfd428ff1c01bb05854bed8a9087b559
Juan Lee <yosohs@gmail.com>
2020-06-05 17:03:26 -0700

    Capture beta

---
+++ SwiftProcessingTesting/SwiftProcessingTesting/SketchCaptureCamera.swift
@@ -0,0 +1,9 @@
+//
+//  SketchCaptureCamera.swift
+//  SwiftProcessingTesting
+//
+//  Created by Juan Lee on 2020-06-05.
+//  Copyright © 2020 Juan Lee. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftProcessingTesting/SwiftProcessingTestingTests/SwiftProcessingTestingTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftProcessingTestingTests.swift
+//  SwiftProcessingTestingTests
+//
+//  Created by Juan Lee on 2020-06-01.
+//  Copyright © 2020 Juan Lee. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftProcessingTesting
+
+class SwiftProcessingTestingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
